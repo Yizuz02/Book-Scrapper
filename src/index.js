a4fbf5b8c014d04af1635106c8743873a97c69ab
Yizuz02 <jchirris04@gmail.com>
2026-08-18 19:27:17 -0600

    Stage 2: discover three catalogue pages

--- src/index.js
+++ src/index.js
@@ -1,22 +1,25 @@
 const fs = require('fs').promises;
+const cheerio = require('cheerio');
 
 const CACHE_FILE = 'cache/page-1.html';
-const URL = 'https://books.toscrape.com/catalogue/page-1.html';
+const BASE_URL = 'https://books.toscrape.com/catalogue/';
 
-async function fecthOrCache() {
-    await fs.mkdir('cache', { recursive: true })
-
+async function fecthOrCache(href, numPage) {
+    await fs.mkdir('cache', { recursive: true });
+    const cacheFile = `cache/page-${numPage}.html`;
     try {
-        const fileContent = await fs.readFile(CACHE_FILE, 'utf-8');
+        const fileContent = await fs.readFile(cacheFile, 'utf-8');
         const size = Buffer.byteLength(fileContent, 'utf-8');
         console.log("CACHE HIT")
         console.log(`Size: ${size} bytes`)
         return fileContent
     } catch (error) {
-        const response = await fetch(URL, {
+        const url = new URL(href, BASE_URL)
+        await new Promise(resolve => setTimeout(resolve, 500));
+        const response = await fetch(url, {
             method  : 'GET', 
             headers : {
-                "User-Agent"   : "FlyRankInternship-A9/1.0 (https://github.com/Yizuz02/Book-Scrapper)"
+                "User-Agent"   : "FlyRankInternship-A9/1.0 (+https://github.com/Yizuz02/Book-Scrapper)"
             },
             signal: AbortSignal.timeout(5000)
         });
@@ -26,7 +29,7 @@ async function fecthOrCache() {
         }
 
         const html = await response.text();
-        await fs.writeFile(CACHE_FILE, html, 'utf-8');
+        await fs.writeFile(cacheFile, html, 'utf-8');
 
         const size = Buffer.byteLength(html, 'utf-8');
         console.log("FETCH")
@@ -35,7 +38,36 @@ async function fecthOrCache() {
     }
 }
 
-fecthOrCache()
+async function main() {
+    let numPage = 1
+    let nextHref = ""
+    const bookUrls = []
+
+    while (numPage<=3) {
+        const html = await fecthOrCache(nextHref, numPage)
+        const $ = cheerio.load(html);
+
+        $('article.product_pod h3 a').each((_, el) => {
+            const href = $(el).attr('href');
+            const absoluteUrl = new URL(href, BASE_URL).href;
+            bookUrls.push(absoluteUrl);
+        });
+
+        const nextRelHref = $('li.next a').attr('href');
+            if (nextRelHref) {
+            nextHref = nextRelHref;
+        }
+
+
+        numPage+=1;
+    }
+
+    const uniqueUrls = Array.from(new Set(bookUrls));
+    console.log(`catalogue_pages=${numPage - 1} discovered=${bookUrls.length} unique_urls=${uniqueUrls.length}`);
+}
+
+main()
+
 
 
 
